64ea82756a00ac3fe28af53780f28fbb2395855c
nashid <noor.nashid1@gmail.com>
2022-06-11 22:56:15 -0700

    fix maven project structure

--- parent-project/app/src/main/java/com/microservice/App.java
+++ parent-project/app/src/main/java/com/microservice/App.java
@@ -2,10 +2,8 @@ package com.microservice;
 
 import com.microservice.resources.ContactResource;
 import io.dropwizard.Application;
-import io.dropwizard.jdbi.DBIFactory;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
-import org.skife.jdbi.v2.DBI;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,10 +16,10 @@ public class App extends Application<PhonebookConfiguration>
 
     @Override
     public void run(PhonebookConfiguration configuration, Environment environment) throws Exception {
-        final DBIFactory factory = new DBIFactory();
-        final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
+        //final DBIFactory factory = new DBIFactory();
+        //final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
 
-        environment.jersey().register(new ContactResource(jdbi));
+        environment.jersey().register(new ContactResource());
     }
 
     public static void main( String[] args ) throws Exception {
--- parent-project/app/src/main/java/com/microservice/resources/ContactResource.java
+++ parent-project/app/src/main/java/com/microservice/resources/ContactResource.java
@@ -7,30 +7,31 @@ import org.skife.jdbi.v2.DBI;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
 import java.util.List;
 
 @Path("/contacts")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 public class ContactResource {
-    private final ContactDao contactDao;
+    //private final ContactDao contactDao;
 
-    public ContactResource(DBI jdbi) {
-        contactDao = jdbi.onDemand(ContactDao.class);
+    public ContactResource() {
+        //contactDao = jdbi.onDemand(ContactDao.class);
     }
 
     @GET
     public Response indexContacts() {
-        List<Contact> contacts = contactDao.getContacts();
-
+        //List<Contact> contacts = contactDao.getContacts();
+        List<Contact> contacts = Arrays.asList();
         return Response.ok(contacts).build();
     }
 
     @GET
     @Path("/{id}")
     public Response showContact(@PathParam("id") int id) {
-        Contact contact = contactDao.getContactById(id);
-
+        // Contact contact = contactDao.getContactById(id);
+        Contact contact = new Contact(1, "john", "doe", "test");
         return Response.ok(contact).build();
     }
 
